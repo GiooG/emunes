27043828f421b0e5a2ac8cbf4306da5afc16b318
Simone Coco <47249685+simuuz@users.noreply.github.com>
2020-05-06 16:56:44 +0200

    Add files via upload

---
+++ emunes/mapper.cpp
@@ -0,0 +1,15 @@
+#include "mapper.h"
+
+mapper::mapper(uint8_t prgBanks, uint8_t chrBanks)
+{
+	nPRGBanks = prgBanks;
+	nCHRBanks = chrBanks;
+
+	reset();
+}
+
+mapper::~mapper()
+{}
+
+void mapper::reset()
+{}
